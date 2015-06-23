5feb7f092e1b26a17b25a0161b501a9082a8fc01
Summer Barton-Taylor <summerbartontaylor@gmail.com>
2015-06-22 19:19:32 -0500

    funtion take var

--- songinput.py
+++ songinput.py
@@ -1,8 +1,11 @@
-def song_input ():
-	song = input('What is your favorite song?')
+a=1
+def song_input (x):
+
 	b=1
 	while b < 21 :
-		print(b,' ',song)
+		print(b,' ',x)
 		b+=1
-
-song_input()
+while a < 11:
+	song = input('What is your favorite song?')
+	song_input(song)
+	a+=1
