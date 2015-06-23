fae762e02798dcd750b05a9ae44df9aa2d766933
Summer Barton-Taylor <summerbartontaylor@gmail.com>
2015-06-22 19:36:26 -0500

    cleaned up functions

--- songinput.py
+++ songinput.py
@@ -1,11 +1,13 @@
-a=1
 def song_input (x):
-
 	b=1
 	while b < 21 :
 		print(b,' ',x)
 		b+=1
-while a < 11:
-	song = input('What is your favorite song?')
-	song_input(song)
-	a+=1
+	
+def song_game():
+	a=1
+	while a < 11:
+		song = input('What is your favorite song?')
+		song_input(song)
+		a+=1
+song_game()
